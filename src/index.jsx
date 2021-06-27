6da7be3ea882d5d80563cb91c8aff3ee3844cb8f
devHudi <devhudi@gmail.com>
2021-06-27 12:33:11 +0900

    feat: add reset style

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 import reportWebVitals from "./reportWebVitals";
 
 import RootRouter from "routers";
 
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <RootRouter />
   </React.StrictMode>,
   document.getElementById("root")
